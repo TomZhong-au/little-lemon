579215a5f0da00eac469b51db67362498d7c725e
Tom <tomzhongcn@gmail.com>
2023-07-26 17:31:52 +0930

    add current page state to navbar to show underline to highlight

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,20 @@
 import { NAVLINKS } from "../../pages/routes";
 import style from "./Navbar.module.css";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+
   return (
     <nav>
       <ul>
         {NAVLINKS.map(({ text, url }) => (
-          <li key={text} className={`${style.navlist} ${style.currentPage}`}>
+          <li
+            key={text}
+            className={`${style.navlist} ${
+              pathname === url ? style.currentPage : ""
+            }`}
+          >
             <a href={url} className="nav-link">
               {text}
             </a>
